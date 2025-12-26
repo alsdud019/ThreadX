f032bb4c48b885056b522261b937116433342d5e
alsdud019 <1926089@gmail.com>
2025-12-26 17:29:13 +0900

    test user.name

--- app/main.c
+++ app/main.c
@@ -6,7 +6,7 @@ void tx_application_define(void *first_unused_memory)
     //스레드 생성 함수 호출
     app_thread_init(first_unused_memory);
     //메모리 블럭 풀 할당 함수 호출
-    app_memory_pool_init();
+    app_memory_pool_init(first_unused_memory);
     //메세지 큐 생성 
 
 
